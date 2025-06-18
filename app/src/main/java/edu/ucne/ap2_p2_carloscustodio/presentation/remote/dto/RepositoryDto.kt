d6bff50d54f7c0b80f8765dd911b2449989bf4fa
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-18 19:32:54 -0400

    Se creo el repositorydto

--- app/src/main/java/edu/ucne/ap2_p2_carloscustodio/presentation/remote/dto/RepositoryDto.kt
+++ app/src/main/java/edu/ucne/ap2_p2_carloscustodio/presentation/remote/dto/RepositoryDto.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class RepositoryDto(
     @Json(name = "name") val name: String,
     @Json(name = "description") val description: String?,
-    @Json(name = "html_url") val htmlUrl: String
+    @Json(name = "html_url") val htmlUrl: String
 )
