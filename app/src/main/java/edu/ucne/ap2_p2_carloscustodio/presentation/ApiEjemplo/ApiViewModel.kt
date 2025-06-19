53d3526d203ad19893b11ec17cd619ba494ca4da
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-18 20:41:00 -0400

    se creo el ApiViewModel.kt

---
+++ app/src/main/java/edu/ucne/ap2_p2_carloscustodio/presentation/ApiEjemplo/ApiViewModel.kt
@@ -0,0 +1,73 @@
+package edu.ucne.ap2_p2_carloscustodio.presentation.ApiEjemplo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.ap2_p2_carloscustodio.data.local.repository.ApiRepository
+import edu.ucne.ap2_p2_carloscustodio.presentation.remote.Resource
+import edu.ucne.ap2_p2_carloscustodio.presentation.remote.dto.RepositoryDto
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ApiViewModel @Inject constructor(
+    private val repository: ApiRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ApiUiState())
+    val uiState: StateFlow<ApiUiState> get() = _uiState
+
+    init {
+        getApi()
+    }
+
+    fun getApi() {
+        viewModelScope.launch {
+            repository.getApi(username = String()).collect { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        _uiState.value = _uiState.value.copy(isLoading = true)
+                    }
+                    is Resource.Success -> {
+                        _uiState.value = _uiState.value.copy(
+                            isLoading = false,
+                            api = result.data ?: emptyList(),
+                            errorMessage = null
+                        )
+                    }
+                    is Resource.Error -> {
+                        _uiState.value = _uiState.value.copy(
+                            isLoading = false,
+                            errorMessage = result.message
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+//    fun saveApi(repositoryDto: RepositoryDto) {
+//        viewModelScope.launch {
+//            val exists = _uiState.value.Api.any { it.name == repositoryDto.name }
+//            if (exists) {
+//                repository.updateApi(repositoryDto)
+//            } else {
+//                repository.createApi(repositoryDto)
+//            }
+//            getApis()
+//        }
+//    }
+
+//    fun deleteApi(name: String) {
+//        viewModelScope.launch {
+//            repository.deleteApi(name)
+//            getApis()
+//        }
+//    }
+
+//    fun getApiByName(name: String?): RepositoryDto? {
+//        return _uiState.value.Api.find { it.name == name }
+//    }
+}
