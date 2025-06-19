1eb5993805eed801400efd7e821c6b75c10a4699
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-06-18 20:15:04 -0400

    se creo el Ap2_P2_CarlosCustodioApp.kt

---
+++ app/src/main/java/edu/ucne/ap2_p2_carloscustodio/Ap2_P2_CarlosCustodioApp.kt
@@ -0,0 +1,7 @@
+package edu.ucne.ap2_p2_carloscustodio
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Ap2_P2_CarlosCustodioApp : Application()
