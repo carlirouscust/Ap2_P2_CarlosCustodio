136688be0eebdd5f41272433df2824ff1ea31046
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-07-09 19:12:11 -0400

    se agrego el apartado para los contribuidores

--- app/src/main/java/edu/ucne/ap2_p2_carloscustodio/presentation/remote/GitHubApi.kt
+++ app/src/main/java/edu/ucne/ap2_p2_carloscustodio/presentation/remote/GitHubApi.kt
@@ -1,5 +1,6 @@
 package edu.ucne.ap2_p2_carloscustodio.presentation.remote
 
+import edu.ucne.ap2_p2_carloscustodio.presentation.remote.dto.ContributorDto
 import edu.ucne.ap2_p2_carloscustodio.presentation.remote.dto.RepositoryDto
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,4 +9,11 @@ import retrofit2.http.Path
 interface GitHubApi {
     @GET("users/enelramon/repos")
     suspend fun listRepos(): Response<List<RepositoryDto>>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun getContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): Response<List<ContributorDto>>
+
 }
